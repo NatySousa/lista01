79f01fd9c278aaa5b44f8bb3b9b9d113d11e6e5a
NatySousa <nataliaspsousa@gmail.com>
2021-05-02 13:20:25 -0300

    versão inicial da projeto

---
+++ Lista01/FuncinarioRepository.cs
@@ -0,0 +1,33 @@
+﻿using Lista01.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+
+namespace Lista01.Repositories
+{
+    public class FuncinarioRepository
+    {
+        public void ExportarDados(Funcionario funcionario)
+        {
+            var path = $"c:\\temp\\funcionario_{funcionario.Id}.txt";
+
+            var streamWriter = new StreamWriter(path);
+
+            streamWriter.WriteLine("\n*** DADOS DO FUNCIONÁRIO *** ");
+            streamWriter.WriteLine("ID......................:" + funcionario.Id);
+            streamWriter.WriteLine("Nome....................:" + funcionario.Nome);
+            streamWriter.WriteLine("Cpf.....................:" + funcionario.Cpf);
+            streamWriter.WriteLine("Matricula...............:" + funcionario.Matricula);
+            streamWriter.WriteLine("Data de Admissão....... :" + funcionario.DataAdmissao);
+            streamWriter.WriteLine("Cargo...................:" + funcionario.Cargo);
+            streamWriter.WriteLine("Salário.................:" + funcionario.Salario);
+
+            streamWriter.Close();
+
+        }
+    }
+}
+
+
---
+++ Lista01/Program.cs
@@ -0,0 +1,66 @@
+﻿using Lista01.Entities;
+using System;
+using Lista01.Repositories;
+
+namespace Lista01.Repositories
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n*** CONTROLE DE FUNCIONÁRIOS ***\n");
+
+            var funcionario = new Funcionario();
+
+            funcionario.Id = Guid.NewGuid();
+
+            Console.Write("Informe o nome do funcionário: ");
+            funcionario.Nome = Console.ReadLine();
+
+            Console.Write("Informe o Cpf do funcionário: ");
+            funcionario.Cpf = Console.ReadLine();
+
+            Console.Write("Matrícula  do funcionário: ");
+            funcionario.Matricula = Console.ReadLine();
+
+            Console.WriteLine("Data de Admissão do funcionário: ");
+            funcionario.DataAdmissao = DateTime.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o cargo do funcionário: ");
+            funcionario.Cargo = Console.ReadLine();
+
+            Console.WriteLine("Informe o salário do funcionário: ");
+            funcionario.Salario = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("\n*** DADOS DO FUNCIONÁRIO ***\n");
+            Console.WriteLine("ID......................:" + funcionario.Id);
+            Console.WriteLine("Nome....................:" + funcionario.Nome);
+            Console.WriteLine("Cpf.....................:" + funcionario.Cpf);
+            Console.WriteLine("Matricula...............:" + funcionario.Matricula);
+            Console.WriteLine("Data de Admissão....... :" + funcionario.DataAdmissao);
+            Console.WriteLine("Cargo...................:" + funcionario.Cargo);
+            Console.WriteLine("Salário.................:" + funcionario.Salario);
+
+            var funcionarioRepository = new FuncinarioRepository();
+
+            try
+            {
+                funcionarioRepository.ExportarDados(funcionario);
+
+                Console.WriteLine("\nDados gravados com sucesso!");
+
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine("\nErro: " + e.Message);
+
+            }
+
+            Console.ReadKey();
+
+
+        }
+
+    }
+}
